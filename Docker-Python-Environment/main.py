45121286e0675a8a6bc5fd845f9cdfc9b43f6272
Alex Johnson <112223622+AeJohnson13@users.noreply.github.com>
2025-11-12 16:16:48 -0900

    Code for training resent50 model

--- Docker-Python-Environment/main.py
+++ Docker-Python-Environment/main.py
@@ -1,4 +1,63 @@
 import torch
+import torchvision
+import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
 
-print("CUDA available:", torch.cuda.is_available())
-print("NCCL available:", torch.distributed.is_nccl_available())
+#checks if cuda is available and uses it if it is
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f'Using {device} for inference')
+
+# Set hyperparameters
+num_epochs = 10
+batch_size = 64
+learning_rate = 0.001
+
+
+# Initialize transformations for data augmentation
+transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomVerticalFlip(),
+    transforms.RandomRotation(degrees=45),
+    transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+training_data = torchvision.datasets.CIFAR10(
+    root="data",
+    train=True,
+    download=True,
+    transform=transform
+)
+
+
+train_loader = torch.utils.data.DataLoader(training_data, batch_size=batch_size, shuffle=True, num_workers=2)
+
+model = torchvision.models.resnet50(weights=ResNet50_Weights.DEFAULT)
+
+model.to(device)
+
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+for epoch in range(num_epochs):
+    for inputs, labels in train_loader:
+        # Move input and label tensors to the device
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        # Zero out the optimizer
+        optimizer.zero_grad()
+
+        # Forward pass
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+
+        # Backward pass
+        loss.backward()
+        optimizer.step()
+
+    # Print the loss for every epoch
+    print(f'Epoch {epoch+1}/{num_epochs}, Loss: {loss.item():.4f}')
