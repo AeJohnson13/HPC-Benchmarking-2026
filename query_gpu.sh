14f27ebde305af46a6e53532fe4dc7185f64d81b
Alex Johnson <aejohnson13@chinookgpu.clstr>
2026-02-25 14:47:33 -0900

    wrote script that records nvidia-smi in background

--- query_gpu.sh
+++ query_gpu.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-condition=
 
-nvidia-smi --query-gpu=timestamp,utilization.gpu,utilization.memory,temperature.gpu --format=csv >> gpu_logs.txt
+job_id=$1
+
+output_file="${job_id}_gpu_logs.txt"
+touch $output_file
+
+running=true
+
+trap 'running=false' SIGTERM
+
+while $running; do
+	nvidia-smi --query-gpu=timestamp,utilization.gpu,utilization.memory,temperature.gpu --format=csv >> "$output_file" 
+	sleep 10
+done
+
