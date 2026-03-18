a85cb8f6130014f9a39d8d359de564f57603bef8
Alex Johnson <112223622+AeJohnson13@users.noreply.github.com>
2026-03-18 13:14:58 -0800

    added debug lines

--- src/ddp_utils.py
+++ src/ddp_utils.py
@@ -10,7 +10,9 @@ import torch.distributed as dist
 
 
 def setup_ddp():
+    print(f"[DEBUG] RANK={os.environ.get('RANK')} LOCAL_RANK={os.environ.get('LOCAL_RANK')}", flush=True)
     dist.init_process_group(backend='nccl')
+    print(f"[DEBUG] init_process_group DONE for rank {os.environ.get('RANK')}", flush=True)
     local_rank = int(os.environ["LOCAL_RANK"])
     global_rank = int(os.environ["RANK"])
     torch.cuda.set_device(local_rank)
