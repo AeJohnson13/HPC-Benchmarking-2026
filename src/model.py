29cad2df132422082805e16b8f2460b61c1195a1
Alex Johnson <aejohnson13@chinookgpu.clstr>
2026-03-19 11:04:01 -0800

    trying to fix

--- src/model.py
+++ src/model.py
@@ -14,7 +14,11 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 def build_model(device, local_rank, use_ddp):
     model = torchvision.models.resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
     # model.fc = nn.Linear(model.fc.in_features, 10)
-    model.to(device)
     if use_ddp == True:
-        model = DDP(model, device_ids=[local_rank])
+        torch.cuda.set_device(local_rank)
+        model = DDP(model, device_ids=[local_rank],output_device=local_rank)
+
+    else:
+        model.to(device)
+
     return model
